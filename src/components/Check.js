7031805a97b56558f690b5b0160f23c08aa1c1c6
Jeff Lau <jeff-lau@live.com>
2017-06-08 19:45:06 +0800

    Got startAuction working

--- src/components/Check.js
+++ src/components/Check.js
@@ -6,15 +6,22 @@ import { ethRegistrar } from '../ensutils'
 const Check = ({ location, match }) => {
   const query = parse(location.search.substr(1))
   const name = query.domain
-  const availability = ethRegistrar.entries(web3.sha3(query.domain))[0].toString()
+  const availability = parseInt(ethRegistrar.entries(web3.sha3(query.domain))[0].toString())
   let content = null
+  console.log(availability)
   switch (availability) {
     case 0:
       content = <div>Domain is available!</div>
+      break;
+    case 1:
+      content = <div>Domain is currently up for auction!</div>
+      break;
     case 5:
       content = <div>Domain is not available to bid yet!</div>
+      break;
     default:
       content = <div>Domain is taken!</div>
+      break;
   }
   return <div>
     <div>{content}</div>
--- src/components/StartAuction.js
+++ src/components/StartAuction.js
@@ -1,9 +1,45 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { parse } from 'qs'
+import { ethRegistrar } from '../ensutils'
+import web3 from '../web3'
+import Loader from 'react-loader'
 
-const StartAuction = ({ match, location }) => {
-  const query = parse(location.search.substr(1))
-  return <div>Started auction for {query.domain}</div>
+class StartAuction extends Component {
+  constructor(){
+    super()
+    this.state = {
+      started: false,
+      pending: false,
+      txId: ""
+    }
+  }
+  render(){
+    let { match, location } = this.props
+    const query = parse(location.search.substr(1))
+    let txId = ""
+    if(this.state.started === false){
+      txId = ethRegistrar.startAuction(web3.sha3(query.domain), {from: web3.eth.accounts[0], gas: 1000000});
+      this.setState({
+        started: true,
+        txId
+      })
+      let interval = setInterval(()=>{
+        let tx = web3.eth.getTransaction(txId)
+        if(tx.blockNumber !== null){
+          this.setState({
+            pending: true,
+            txId
+          })
+          clearInterval(interval)
+        }
+      }, 1000)
+    }
+
+    return <div>
+      Started auction for {query.domain} with the txId <a href={`https://ropsten.etherscan.io/tx/${this.state.txId}`}>{this.state.txId}</a>
+      {this.state.pending ? <div>Transaction has been mined and auction has been started!</div> : <div>Waiting for Transaction to be mined<Loader /></div>}
+    </div>
+  }
 }
 
 export default StartAuction
